6e6e2338136c8fdae5726a576ad9ce6269fa4767
Massatrio16 <dimassetosatrio@gmail.com>
2025-07-02 13:57:22 +0700

    tes

--- device.mk
+++ device.mk
@@ -69,8 +69,7 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 
 # A/B
 
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(LOCAL_PATH)/security/lineage \
+
 
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
