d4f878221f00e27c6ed5356dc03b03d1b0f7f6f8
Massatrio16 <dimassetosatrio@gmail.com>
2025-07-09 06:45:40 +0700

    initial

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The TWRP Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,67 +12,64 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-LOCAL_PATH := device/realme/rmx3265
 
+LOCAL_PATH := device/realme/RMX3261
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# A/B
+TARGET_IS_VAB := true
+ENABLE_VIRTUAL_AB := true
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Boot control HAL
+# f2fs utilities
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.2-service \
-    vendor.sprd.hardware.boot@1.2-service
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs
+    
+# Userdata checkpoint
+PRODUCT_PACKAGES += \
+    checkpoint_gc
 
-# Health HAL
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true    
+
+# Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# bootctrl HAL    
+PRODUCT_PACKAGES += \
+    bootctrl.default \
+    bootctrl.unisoc \
+    bootctrl.ums512.recovery
  
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl-recovery 
-
-
-# Dynamic Partition
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-PRODUCT_PACKAGES += \
-    bootctrl.ums512
-    
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client 
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_verifier \
-    update_engine_sideload
+    update_engine_sideload \
+    checkpoint_gc 
 
-
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 30
-PRODUCT_SHIPPING_API_LEVEL := 29
-
-# A/B
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(LOCAL_PATH)/security/releasekey \
-    $(LOCAL_PATH)/security/fsverity-release
-
-
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl    
+    
+# Fastbootd
 PRODUCT_PACKAGES += \
+    fastbootd \
     android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Hidl
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
--- twrp_rmx3265.mk
+++ twrp_rmx3265.mk
@@ -6,14 +6,26 @@
 #
 DEVICE_PATH := device/realme/rmx3265
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Enable virtual A/B OTA
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Enable virtual A/B OTA
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Inherit from itel device
-$(call inherit-product, device/realme/rmx3265/device.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product-if-exists, vendor/twrp/config/common.mk)
+
+# Inherit some common pb stuff.
+$(call inherit-product-if-exists, vendor/pb/config/common.mk)
+
+# Inherit from RMX3261 device
+$(call inherit-product, device/realme/RMX3261/device.mk)
+
 
 PRODUCT_DEVICE := rmx3265
 PRODUCT_NAME := twrp_rmx3265
